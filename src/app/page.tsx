5e3e8433b85ed7a71f994cc6abb9718fba90c012
Santiago Aguirre Giraldo <santiagowe98@gmail.com>
2024-03-18 12:24:16 -0500

    Current and coming sections created in home page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,4 +1,5 @@
 import { Metadata } from "next";
+import Image from "next/image";
 
 export const metadata: Metadata = {
   title: "Cinema",
@@ -10,6 +11,8 @@ export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
       <Intro />
+      <CurrentMoviesSection />
+      <ComingMoviesSection />
     </main>
   );
 }
@@ -17,8 +20,136 @@ export default function Home() {
 function Intro() {
   return (
     <>
-      <h1>Welcome to our cinema</h1>
+      <h1 className="font-bold">Welcome to our cinema</h1>
       <p>Explore and see all movies you want</p>
+    </>
+  );
+}
+
+function CurrentMoviesSection() {
+  return (
+    <>
+      <h1 className="font-bold">Current movies</h1>
+      <ul>
+        <li>
+          <p>Movie name</p>
+          <Image
+            src="/images/cinema-current.jpg"
+            alt="Picture of cinema things"
+            width={100}
+            height={100}
+          />
+        </li>
+        <li>
+          <p>Movie name</p>
+          <Image
+            src="/images/cinema-current.jpg"
+            alt="Picture of cinema things"
+            width={100}
+            height={100}
+          />
+        </li>
+        <li>
+          <p>Movie name</p>
+          <Image
+            src="/images/cinema-current.jpg"
+            alt="Picture of cinema things"
+            width={100}
+            height={100}
+          />
+        </li>
+        <li>
+          <p>Movie name</p>
+          <Image
+            src="/images/cinema-current.jpg"
+            alt="Picture of cinema things"
+            width={100}
+            height={100}
+          />
+        </li>
+        <li>
+          <p>Movie name</p>
+          <Image
+            src="/images/cinema-current.jpg"
+            alt="Picture of cinema things"
+            width={100}
+            height={100}
+          />
+        </li>
+        <li>
+          <p>Movie name</p>
+          <Image
+            src="/images/cinema-current.jpg"
+            alt="Picture of cinema things"
+            width={100}
+            height={100}
+          />
+        </li>
+      </ul>
+    </>
+  );
+}
+
+function ComingMoviesSection() {
+  return (
+    <>
+      <h1 className="font-bold">Coming movies</h1>
+      <ul>
+        <li>
+          <p>Movie name</p>
+          <Image
+            src="/images/cinema-coming.jpg"
+            alt="Picture of cinema things"
+            width={100}
+            height={100}
+          />
+        </li>
+        <li>
+          <p>Movie name</p>
+          <Image
+            src="/images/cinema-coming.jpg"
+            alt="Picture of cinema things"
+            width={100}
+            height={100}
+          />
+        </li>
+        <li>
+          <p>Movie name</p>
+          <Image
+            src="/images/cinema-coming.jpg"
+            alt="Picture of cinema things"
+            width={100}
+            height={100}
+          />
+        </li>
+        <li>
+          <p>Movie name</p>
+          <Image
+            src="/images/cinema-coming.jpg"
+            alt="Picture of cinema things"
+            width={100}
+            height={100}
+          />
+        </li>
+        <li>
+          <p>Movie name</p>
+          <Image
+            src="/images/cinema-coming.jpg"
+            alt="Picture of cinema things"
+            width={100}
+            height={100}
+          />
+        </li>
+        <li>
+          <p>Movie name</p>
+          <Image
+            src="/images/cinema-coming.jpg"
+            alt="Picture of cinema things"
+            width={100}
+            height={100}
+          />
+        </li>
+      </ul>
     </>
   );
 }
